f33c070f69fb0fd11fd1e551c7c0b5f2ceb93522
Sanjay R <sanjay14112000@gmail.com>
2023-11-23 23:41:21 -0800

    check-duplicates

--- analyse-results.py
+++ analyse-results.py
@@ -1,11 +1,13 @@
 import pickle
 import pandas as pd
+import os
+import glob
 
 from collections import defaultdict
 from get_hash import generate_frame_hash
 from read_rgb import read_frame
 
-hash_values = pickle.load(open('./hash-values/video1.pkl', 'rb'))
+
 
 def test(video_path, hash_values):
     video_reader = read_frame()(video_path)
@@ -17,4 +19,32 @@ def test(video_path, hash_values):
                 print(entry)
                 return
 
-print(test('/Users/sanjay/Downloads/576_final_PA/Queries/video1_1.rgb', hash_values))
+def check_duplicates(root):
+    for pkl_file in glob.glob(os.path.join(root, '**/*.pkl'), recursive=True):
+        hash_values = pickle.load(open(pkl_file, 'rb'))
+        hash_set = defaultdict(list)
+
+        for entry in hash_values:
+            hash_set[entry['hash']].append(entry['frame_number'])
+
+        print_flag = False
+        for h, f in hash_set.items():
+            if len(f) > 1:
+                if not print_flag:
+                    print(f'Duplicates found in {pkl_file}: ')
+                    print_flag = True
+                print(f'Frames with same values: {f}')
+        print()
+    return
+
+
+def main():
+
+    check_duplicates('./hash-values/')
+
+    # Code to run query
+    # hash_values = pickle.load(open('./hash-values/video1.pkl', 'rb'))
+    # print(test('/Users/sanjay/Downloads/576_final_PA/Queries/video1_1.rgb', hash_values))
+
+if __name__ == '__main__':
+    main()
